c1f44156b9ac4a87a9f922e86b0d56d3f8396a6c
zbys1av <zbyslav13@gmail.com>
2021-01-21 16:40:57 +0200

    working but neet to make with State

--- pass/src/App.js
+++ pass/src/App.js
@@ -1,64 +1,56 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [userName, setName] = useState('');
 
-  // let value = "Megazby";
-  // let name = "zbyslav";
-  // name = name.match(/(.{1,2})/g);
+  let value = title;
+  let name = userName;
+  name = name.match(/(.{1,2})/g);
 
-  // function firstStap (){
-  //   value = value.substr(3, value.length) + value.substr(0, 3);
-  //   secondStap();
-  //   return value;
-  // }
+  function firstStap (){
+    value = value.substr(3, value.length) + value.substr(0, 3);
+    secondStap();
+    return value;
+  }
 
-  // function secondStap(){
-  //   value += value.length;
-  //   thirdStap();
-  //   return value;
-  // }
+  function secondStap(){
+    value += value.length;
+    thirdStap();
+    return value;
+  }
 
-  // function thirdStap(){
-  //   value = value.replace("e", "3");
-  //   fourthSrap();
-  //   return value;
-  // }
+  function thirdStap(){
+    value = value.replace("e", "3");
+    fourthSrap();
+    return value;
+  }
 
-  // function fourthSrap(){
-  //     value = value.substring(0, 2) + name[0] + value.substring(2);
-  //     value = value.substring(0, 6) + name[1] + value.substring(6);
-  //     value = value.substring(0, 10) + name[2] + value.substring(10);
-  //     value = value.substring(0, 14) + name[3] + value.substring(14);
-  // }
-
-  // console.log(firstStap());
-
-  function myResult(){
-
+  function fourthSrap(){
+    value = value.substring(0, 2) + name[0] + value.substring(2);
+    value = value.substring(0, 6) + name[1] + value.substring(6);
+    value = value.substring(0, 10) + name[2] + value.substring(10);
+    value = value.substring(0, 14) + name[3] + value.substring(14);
+    // return value;
   }
 
   return (
     <div className="App">
       <div className="data">
         <div className="inputs">
-          {/* <p className="input-text"> */}
-            <input className="input" type="input" placeholder="For what"></input>
-            {/* For what */}
-          {/* </p> */}
+            <input className="input" onChange={event => setTitle(event.target.value)} type="input" placeholder="For what"></input>
         </div>
         <div className="inputs">
-          {/* <p className="input-text"> */}
-            <input className="input" type="input" placeholder="Login"></input>
-          {/* Login */}
-          {/* </p> */}
+            <input className="input" onChange={event => setName(event.target.value)} type="input" placeholder="Login"></input>
         </div>
       </div>
       <div className="buttons">
-        <button className="button-create" onClick={myResult}>Create</button>
+        <button type="button" className="button-create">Create</button>
         <button className="button-clear">Clear</button>
       </div>
-      <div class="results">
-        <input className="result" placeholder="Result" readOnly></input>
+      <div className="results">
+        <input className="result" placeholder="Result" value={firstStap(value)} readOnly></input>
       </div>
     </div>
   );
